88d4406c462e53d8baa037b0552dbd8a4a40bb85
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2021-03-02 18:52:34 +0100

    leeo

--- server/index.js
+++ server/index.js
@@ -21,31 +21,41 @@ io.on('test', () => {
 })
 
 io.on('connection', function(socket) {
-    console.log(`a user ${playerID} connected`);
-    socket.emit("io emit", { msg:`Welcome, player ${playerID}!`, newPlayerID:playerID });
-   // playerID=playerID*-1;
-    playerID++;
-    socket.on("socket emit", () => {
-        io.emit("io emit-two", "The server noticed that you clicked something!");
-    });
+  console.log(`a user ${playerID} connected`);
+  socket.emit("io emit", { msg:`Welcome, player ${playerID}!`, newPlayerID:playerID });
+ // playerID=playerID*-1;
+  playerID++;
+  socket.on("socket emit", () => {
+      io.emit("io emit-two", "The server noticed that you clicked something!");
+  });
 
-    socket.on('disconnect', () => {
-      console.log("someone disconnected.")
-      playerID=playerID-1;
-    });
+  socket.on('disconnect', () => {
+    console.log("someone disconnected.")
+    playerID=playerID-1;
+  });
 
+  socket.on('join room', (roomName) => {
+    socket.join(roomName);
+    console.log("someon joined a room!", roomName)
 
+  })
 
-    //Sets currentPlayer to next
-    socket.on('next player', ({ currentPlayer, board }) => {
+  socket.on('send ping', (roomName) => {
+    console.log("ping sent!")
+    socket.broadcast.to(roomName).emit('ping received');
+  })
 
-      console.log("next player!1", currentPlayer);
-      currentPlayer == 1 ? currentPlayer = 2 : currentPlayer = 1;
-      console.log("next player!2", currentPlayer);
-     // console.log(board);
-      socket.broadcast.emit('new player', { newCurrentPlayer: currentPlayer, newBoard: board })
-    });
+  //Sets currentPlayer to next
+  socket.on('next player', ({ currentPlayer, board, roomName }) => {
+
+    console.log("next player!1", currentPlayer, roomName);
+    currentPlayer == 1 ? currentPlayer = 2 : currentPlayer = 1;
+    console.log("next player!2", currentPlayer);
+   // console.log(board);
+    socket.broadcast.to(roomName).emit('new player', { newCurrentPlayer: currentPlayer, newBoard: board })
+  });
 });
+
 
 
 
