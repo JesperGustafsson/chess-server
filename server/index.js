e8f26ae6f531bb99a155f7c39320a6aa97f6e601
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2021-03-02 13:55:16 +0100

    koi

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-/*   , {
+const io = require('socket.io')(http
+  , {
   cors: {
-      origin: ["https://hungry-franklin-438ea6.netlify.app/", "http://localhost:3000"] //needed so that port ;3000 can connect to the server at ;4000
+      origin: true//["https://hungry-franklin-438ea6.netlify.app/", "http://localhost:3000"] //needed so that port ;3000 can connect to the server at ;4000
   },
-}); */
+});
 app.use(cors());
 var playerID = 1;
 
