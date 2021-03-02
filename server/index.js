a5f05ed1707f9b563ed94fe155fe661df6ff64b5
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2021-03-02 13:38:45 +0100

    koi

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-      origin: "https://hungry-franklin-438ea6.netlify.app/" //needed so that port ;3000 can connect to the server at ;4000
+      origin: ["https://hungry-franklin-438ea6.netlify.app/", "http://localhost:3000"] //needed so that port ;3000 can connect to the server at ;4000
   },
 });
 
